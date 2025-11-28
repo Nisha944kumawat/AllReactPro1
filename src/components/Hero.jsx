a80b750da64fd178122fedf90f6e38bbf0c6deec
Nisha Kumawat <nishakumawat944@gmail.com>
2025-11-28 09:28:29 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,24 @@
-const HeroSection=()=>{
-    return <>
+import flipkartLogo from '../assets/flipkart.png';
+import amazonLogo from '../assets/amazon.png';
+import shoeImage from '../assets/shoe_image.png';
+
+const HeroSection = () => {
+    return (
         <main className="hero container">
             <div className="hero-content">
                 <h1>YOUR FEET DESERVE THE BEST</h1>
-                <p>YOUR FEET DESERVE THE BEST AND WE’RE 
-                HERE TO HELP YOU WITH OUR SHOES.YOUR FEET 
-                DESERVE THE BEST AND WE’RE HERE TO HELP YOU 
-                WITH OUR SHOES.</p>
 
-               <div className="hero-btn">
-                <button>Shop Now</button>
-                <button className="secondary-btn">Category</button>
-               </div>
-             <div className="brand-icons">
-                <img src="/images/flipkart.png" alt="flipkart-logo" />
-                <img src="/images/amazon.png" alt="amazon-logo" />
-            </div>
-               <div className="shopping">
-                <p>Also Available On</p>
-               </div>
+                <div className="brand-icons">
+                    <img src={flipkartLogo} alt="flipkart" />
+                    <img src={amazonLogo} alt="amazon" />
+                </div>
+
             </div>
 
-             <div className="hero-image">
-                <img src="/images/shoe_image.png" alt="shoe-logo" />
-             
-
+            <div className="hero-image">
+                <img src={shoeImage} alt="shoe" />
             </div>
         </main>
-    </>;
-}
+    );
+};
 export default HeroSection;
