8a57417172038e1686d8fa7b87fac23652ce6c20
Nisha Kumawat <kumawatnisha9091@gmail.com>
2025-11-28 09:39:40 +0530

    Move images from public/images to src/assets

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,41 @@
-const HeroSection=()=>{
-    return <>
-        <main className="hero container">
-            <div className="hero-content">
-                <h1>YOUR FEET DESERVE THE BEST</h1>
-                <p>YOUR FEET DESERVE THE BEST AND WE’RE 
-                HERE TO HELP YOU WITH OUR SHOES.YOUR FEET 
-                DESERVE THE BEST AND WE’RE HERE TO HELP YOU 
-                WITH OUR SHOES.</p>
+import flipkartLogo from '../assets/flipkart.png';
+import amazonLogo from '../assets/amazon.png';
+import shoeImage from '../assets/shoe_image.png';
 
-               <div className="hero-btn">
-                <button>Shop Now</button>
-                <button className="secondary-btn">Category</button>
-               </div>
-             <div className="brand-icons">
-                <img src="/images/flipkart.png" alt="flipkart-logo" />
-                <img src="/images/amazon.png" alt="amazon-logo" />
-            </div>
-               <div className="shopping">
-                <p>Also Available On</p>
-               </div>
-            </div>
+const HeroSection = () => {
+    return (
+        <>
+            <main className="hero container">
+                <div className="hero-content">
+                    <h1>YOUR FEET DESERVE THE BEST</h1>
+                    <p>
+                        YOUR FEET DESERVE THE BEST AND WE’RE 
+                        HERE TO HELP YOU WITH OUR SHOES. YOUR FEET 
+                        DESERVE THE BEST AND WE’RE HERE TO HELP YOU 
+                        WITH OUR SHOES.
+                    </p>
 
-             <div className="hero-image">
-                <img src="/images/shoe_image.png" alt="shoe-logo" />
-             
+                    <div className="hero-btn">
+                        <button>Shop Now</button>
+                        <button className="secondary-btn">Category</button>
+                    </div>
 
-            </div>
-        </main>
-    </>;
-}
+                    <div className="brand-icons">
+                        <img src={flipkartLogo} alt="flipkart-logo" />
+                        <img src={amazonLogo} alt="amazon-logo" />
+                    </div>
+
+                    <div className="shopping">
+                        <p>Also Available On</p>
+                    </div>
+                </div>
+
+                <div className="hero-image">
+                    <img src={shoeImage} alt="shoe-logo" />
+                </div>
+            </main>
+        </>
+    );
+};
+
 export default HeroSection;
