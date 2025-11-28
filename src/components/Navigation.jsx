eec7325406334a06de226b9b0bff60dab985b18f
Nisha Kumawat <nishakumawat944@gmail.com>
2025-11-28 09:32:04 +0530

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,24 @@
-const Navigation=()=>{
-return(
+import brandLogo from "../assets/brand_logo.png";
+
+const Navigation = () => {
+  return (
     <div>
-    <nav className="container">
-      <div  className="logo">
-        <img src="/images/brand_logo.png" alt="logo"/>
-      </div>
-      <ul>
-      <li href="#">Menu</li>
-      <li href="#">Location</li>
-      <li href="#">About</li>
-      <li href="#">Contact</li>
-      </ul>
-      <button>Login</button>
-     
-    </nav>
-  </div>
-);
-}
+      <nav className="container">
+        <div className="logo">
+          <img src={brandLogo} alt="logo" />
+        </div>
+
+        <ul>
+          <li href="#">Menu</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </div>
+  );
+};
+
 export default Navigation;
